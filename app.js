e6a18b4958da11c37892f4e46e90b3db92987ad3
Mykola Skorenkyi <mskorenkyi@corevalue.net>
2017-02-17 16:53:05 +0200

    added filex

---
+++ app.js
@@ -0,0 +1,412 @@
+
+var graph = {
+		"nodes": [
+			{
+				"id": "Myriel",
+				"type": 1,
+				"name": "Myriel hospital",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Napoleon",
+				"type": 1,
+				"name": "Napoleon hospital",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Mlle.Baptistine",
+				"type": 0,
+				"name": "Mlle.User Baptistine",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Mme.Magloire",
+				"type": 0,
+				"name": "Mme.User Magloire",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "CountessdeLo",
+				"type": 0,
+				"name": "User CountessdeLo",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Geborand",
+				"type": 0,
+				"name": "User Geborand",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Champtercier",
+				"type": 1,
+				"name": "Champtercier hospital",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Cravatte",
+				"type": 0,
+				"name": "User Cravatte",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Count",
+				"type": 0,
+				"name": "User Count",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "OldMan",
+				"type": 0,
+				"name": "User OldMan",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Labarre",
+				"type": 0,
+				"name": "User Labarre",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Valjean",
+				"type": 0,
+				"name": "User Valjean",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Marguerite",
+				"type": 0,
+				"name": "User Marguerite",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Mme.deR",
+				"type": 0,
+				"name": "Mme.User deR",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Isabeau",
+				"type": 0,
+				"name": "User Isabeau",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Gervais",
+				"type": 0,
+				"name": "User Gervais",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Tholomyes",
+				"type": 0,
+				"name": "User Tholomyes",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Listolier",
+				"type": 0,
+				"name": "User Listolier",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Fameuil",
+				"type": 0,
+				"name": "User Fameuil",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Blacheville",
+				"type": 0,
+				"name": "User Blacheville",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Favourite",
+				"type": 0,
+				"name": "User Favourite",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Dahlia",
+				"type": 0,
+				"name": "User Dahlia",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			},
+			{
+				"id": "Zephine",
+				"type": 0,
+				"name": "User Zephine",
+				"age": "23",
+				"phone": "(555)777-77-77",
+				"email": "example@email.com",
+				"avatar": ""
+			}
+		],
+		"links": [
+			{"source": "Napoleon", "target": "Myriel"},
+			{"source": "Mlle.Baptistine", "target": "Myriel"},
+			{"source": "Mme.Magloire", "target": "Myriel"},
+			{"source": "Mme.Magloire", "target": "Mlle.Baptistine"},
+			{"source": "CountessdeLo", "target": "Myriel"},
+			{"source": "CountessdeLo", "target": "Blacheville"},
+			{"source": "Geborand", "target": "Myriel"},
+			{"source": "Champtercier", "target": "CountessdeLo"},
+			{"source": "Isabeau", "target": "Champtercier"},
+			{"source": "Zephine", "target": "Champtercier"},
+			{"source": "OldMan", "target": "Champtercier"},
+			{"source": "Gervais", "target": "Champtercier"},
+			{"source": "Valjean", "target": "Champtercier"},
+			{"source": "Tholomyes", "target": "Valjean"},
+			{"source": "Dahlia", "target": "Count"},
+			{"source": "Count", "target": "Mme.deR"},
+			{"source": "Cravatte", "target": "Mme.deR"},
+			{"source": "Myriel", "target": "Mme.deR"},
+			{"source": "Napoleon", "target": "Listolier"},
+			{"source": "Napoleon", "target": "Marguerite"},
+			{"source": "Napoleon", "target": "Favourite"},
+			{"source": "Napoleon", "target": "Labarre"},
+			{"source": "Napoleon", "target": "Fameuil"}
+		]
+	},
+	defaultUser = 'https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/boy-512.png',
+	defaultHospital = 'https://cdn2.iconfinder.com/data/icons/medicine-9/512/hospital-512.png',
+	width = document.body.clientWidth,
+	height = 600;
+
+var svg = d3.select("svg")
+		.attr("width", width)
+		.attr("height", height),
+	shiftKey, ctrlKey, selected;
+
+var defs = svg.append('svg:defs');
+
+d3.select('#affiliation')
+	.attr("tabindex", 1)
+	.on("keydown.brush", keydown)
+	.on("keyup.brush", keyup)
+	.each(function() { this.focus(); });
+
+addImage('user', defaultUser);
+addImage('hospital', defaultHospital);
+
+var simulation = d3.forceSimulation()
+	.force("link", d3.forceLink().id(function(d) { return d.id; }))
+	.force("charge", d3.forceManyBody())
+	.force("center", d3.forceCenter(width / 2, height / 2));
+
+var link = svg.append("g")
+	.attr("class", "links")
+	.selectAll("line")
+	.data(graph.links)
+	.enter()
+	.append('g').append("line")
+	.attr("stroke-width", 1);
+
+var cont = svg.append("g")
+	.attr("class", "nodes");
+
+var node = cont
+	.selectAll("circle")
+	.data(graph.nodes)
+	.enter().append('g')
+	.on('mouseover', function () {
+		event.currentTarget.parentElement.append(event.currentTarget)
+	})
+	.on('click', function (d) {
+		if (d3.event.defaultPrevented) return;
+		var state = d.selected;
+		var _selected = [];
+		if (!shiftKey) {
+			node.classed("selected", function(p) { return p.selected = false; })
+		}
+
+		d.selected = !state;
+		node.each(function (_d) {
+			if (_d.selected) {
+				_selected.push(_d);
+			}
+		});
+		if (_selected.length === 1) {
+			selected = _selected[0];
+		} else {
+			selected = null;
+		}
+		onChangeSelectedNode();
+		d3.select(this).classed("selected", d.selected);
+	});
+
+node.append("circle")
+	.attr('class','wrapper')
+	.attr("r", 20);
+
+node.append("circle")
+	.attr('class','avatar')
+	.attr("r", 18)
+	.attr("fill", function (d) {
+		var icon;
+		if (d.avatar) {
+			//add to svg
+			addImage('user-'+d.id, d.avatar);
+			icon = 'user-'+d.id;
+		} else {
+			switch (d.type) {
+				case 0:
+					icon = 'user';
+					d.avatar = defaultUser;
+					break;
+				case 1:
+					icon = 'hospital';
+					d.avatar = defaultHospital;
+					break;
+				default:
+					icon = 'user';
+					d.avatar = defaultUser;
+
+			}
+		}
+
+
+		return 'url(#'+icon+')';
+	});
+
+node.append("text")
+	.text(function(d) { return d.name; })
+	.attr('text-anchor', 'middle')
+	.attr('x', '0')
+	.attr('y', '35');
+
+simulation
+	.nodes(graph.nodes)
+	.on("tick", ticked);
+
+
+var _f = d3.forceCollide();
+_f.radius(40)
+	.strength(1)
+	.iterations(10)
+	.initialize(graph.nodes);
+
+_f();
+
+simulation.force("link")
+	.distance(function () {
+		return 100;
+	})
+	.links(graph.links);
+
+
+function ticked() {
+	link
+		.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+	node
+		.attr("transform", function (d) {
+			return 'translate(' + d.x + ',' + d.y + ')'
+		});
+}
+
+function addImage(id,url) {
+	defs.append('svg:pattern')
+		.attr('id', id)
+		.attr('x', '0%')
+		.attr('y', '0%')
+		.attr('width', '100%')
+		.attr('height', '100%')
+		.attr('viewBox', '0 0 32 32')
+		.append('svg:image')
+		.attr('xlink:href', url)
+		.attr('x', '0%')
+		.attr('y', '0%')
+		.attr('width', 32)
+		.attr('height', 32);
+}
+
+function keydown() {
+	shiftKey = d3.event.shiftKey || d3.event.metaKey;
+	ctrlKey = d3.event.ctrlKey;
+}
+
+function keyup() {
+	shiftKey = d3.event.shiftKey || d3.event.metaKey;
+	ctrlKey = d3.event.ctrlKey;
+}
+
+function onChangeSelectedNode() {
+	var container = document.getElementById('container');
+	var html = "";
+	if (selected) {
+		var tmpl = document.getElementById('tmpl');
+		html = _.template(tmpl.innerHTML)({data:selected});
+	}
+	container.innerHTML = html;
+}
+
